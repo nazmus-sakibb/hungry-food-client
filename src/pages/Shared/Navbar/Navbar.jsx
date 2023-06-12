fb9122ae02e013ddbeacd528b95527065e11de55
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-06-13 03:30:38 +0600

    create banner, setup carousel

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
 
-
 const Navbar = () => {
 
     const navMenus = <>
         <li><a>Item 1</a></li>
-        <li>
-            <a>Parent</a>
-            <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-            </ul>
+        <li tabIndex={0}>
+            <details>
+                <summary>Parent</summary>
+                <ul className="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+                </ul>
+            </details>
         </li>
         <li><a>Item 3</a></li>
     </>
 
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar max-w-screen-xl fixed z-10 bg-opacity-30 bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
