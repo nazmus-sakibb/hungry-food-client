6eb1a50d0ff2703ed4493eb9833981830fbd5cbb
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-06-13 21:16:09 +0600

    featured background image modified

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -1,15 +1,17 @@
 import featured from '../../../assets/home/featured.jpg';
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import './Featured.css';
 
 
 const Featured = () => {
     return (
-        <div>
+        <div className='featured-item bg-fixed mb-16 text-white'>
+            <div className='bg-opacity-50 bg-black py-12'>
             <SectionTitle
                 subHeading='Check It Out'
                 heading='Featured Item'
             />
-            <div className='md:flex justify-evenly items-center py-10'>
+            <div className='md:flex justify-evenly items-center py-5'>
                 <div className=''>
                     <img className='md:w-[450px]' src={featured} alt="" />
                 </div>
@@ -17,8 +19,9 @@ const Featured = () => {
                     <p>Aug 20, 2029</p>
                     <p className="uppercase">Where can I get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, consequatur hic, laudantium officiis expedita corrupti ipsum magnam animi sit, unde modi itaque possimus eos molestias quisquam iusto sapiente dolorem? Numquam officia autem quae libero, iusto nostrum qui eum tempora aut! Optio vitae quisquam odit cumque saepe quibusdam cupiditate officiis a!</p>
-                    <button className="btn btn-outline">Read More</button>
+                    <button className="btn btn-outline text-white border-0 border-b-2 mt-5">Read More</button>
                 </div>
+            </div>
             </div>
         </div>
     );
