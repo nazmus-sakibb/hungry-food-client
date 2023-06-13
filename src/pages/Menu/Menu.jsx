c156ba0a8354f1d1c1d6d1f1461aa67e4a70479b
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-06-13 23:58:31 +0600

    implement cover with react parallax

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from 'react-helmet-async';
+import menuBanner from '../../assets/menu/banner-bg.jpg';
+import Cover from '../Shared/Cover/Cover';
+import PopularMenu from '../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -6,7 +9,26 @@ const Menu = () => {
             <Helmet>
                 <title>HungryFood | Menu</title>
             </Helmet>
-            <h2>Menu</h2>
+            <Cover
+                img={menuBanner}
+                title={'Our Menu'}
+            />
+            <PopularMenu />
+            <Cover
+                img={menuBanner}
+                title={'Our Menu'}
+            />
+            <PopularMenu />
+            <Cover
+                img={menuBanner}
+                title={'Our Menu'}
+            />
+            <PopularMenu />
+            <Cover
+                img={menuBanner}
+                title={'Our Menu'}
+            />
+            <PopularMenu />
         </div>
     );
 };
