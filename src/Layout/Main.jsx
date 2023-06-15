0fcec2d0e36ece220a24c166ccab26579f0faa29
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-06-15 20:47:45 +0600

    create login form

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,6 +4,10 @@ import Navbar from "../pages/Shared/Navbar/Navbar";
 
 
 const Main = () => {
+    // const location = useLocation();
+    // console.log(location);
+    // const isLogin = location.pathname.includes('login');
+
     return (
         <div>
             <Navbar />
