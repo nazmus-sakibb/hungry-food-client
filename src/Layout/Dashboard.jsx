563235352b3953384bd1b5cd98c45e9106d6be66
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-07-04 18:41:46 +0600

    limit the feature based on admin level

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,14 +1,15 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { FaBook, FaCalendarAlt, FaHome, FaShoppingCart, FaUsers, FaUtensils, FaWallet } from 'react-icons/fa';
 import useCart from "../hooks/useCart";
+import useAdmin from "../hooks/useAdmin";
 
 
 const Dashboard = () => {
     const [cart] = useCart();
 
     // TODO - load data from the server to have dynamic isAdmin based on data
-    const isAdmin = true;
-
+    // const isAdmin = true;
+    const [isAdmin] = useAdmin();
 
 
     return (
