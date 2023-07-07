e65f4a1638cd7458d7a6d3e7b415064e4b918855
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-07-07 20:33:06 +0600

    where to host images with pros and cons

--- src/pages/Dashboard/AddItem/AddItem.jsx
+++ src/pages/Dashboard/AddItem/AddItem.jsx
@@ -13,44 +13,46 @@ const AddItem = () => {
         <div className="w-full">
             <SectionTitle subHeading="What's new" heading="Add an Item" />
             <form className="ml-20" onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-control w-full max-w-xs">
+                <div className="form-control w-full mb-4">
                     <label className="label">
                         <span className="label-text font-semibold">Recipe name*</span>
                     </label>
                     <input type="text" placeholder="Recipe Name"
                         {...register("name", { required: true, maxLength: 120 })}
-                        className="input input-bordered w-full max-w-xs" />
+                        className="input input-bordered w-full" />
                 </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Category*</span>
-                    </label>
-                    <select {...register("category", { required: true })} className="select select-bordered">
-                        <option disabled selected>Pick one</option>
-                        <option>Pizza</option>
-                        <option>Soup</option>
-                        <option>Salad</option>
-                        <option>Drinks</option>
-                        <option>Dessert</option>
-                    </select>
+                <div className="flex justify-between mb-4">
+                    <div className="form-control w-full pr-3">
+                        <label className="label">
+                            <span className="label-text font-semibold">Category*</span>
+                        </label>
+                        <select {...register("category", { required: true })} className="select select-bordered">
+                            <option disabled selected>Pick one</option>
+                            <option>Pizza</option>
+                            <option>Soup</option>
+                            <option>Salad</option>
+                            <option>Drinks</option>
+                            <option>Dessert</option>
+                        </select>
+                    </div>
+                    <div className="form-control w-full pl-3">
+                        <label className="label">
+                            <span className="label-text font-semibold">Price*</span>
+                        </label>
+                        <input type="number" {...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full" />
+                    </div>
                 </div>
-                <div className="form-control w-full max-w-xs">
+                <div className="form-control mb-4">
                     <label className="label">
-                        <span className="label-text font-semibold">Price*</span>
+                        <span className="label-text font-semibold">Recipe Details</span>
                     </label>
-                    <input type="number" {...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    <textarea {...register("recipe", { required: true })} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
                 </div>
-                <div className="form-control">
+                <div className="form-control w-full mb-4">
                     <label className="label">
-                        <span className="label-text">Recipe Details</span>
+                        <span className="label-text font-semibold">Item Image*</span>
                     </label>
-                    <textarea {...register("details", { required: true })} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Item Image*</span>
-                    </label>
-                    <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
+                    <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full max-w-xs" />
                 </div>
                 <input type="submit" value="Add Item" className="btn btn-sm btn-neutral mt-4" />
             </form>
