7e40834e81e99053c57605c87be268cb11be6b84
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-07-09 16:01:59 +0600

    get price for payment intent and use array reduce

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -4,20 +4,11 @@ import useAuth from './useAuth';
 
 const useCart = () => {
     const { user, loading } = useAuth();
-    // const token = localStorage.getItem('access-token');
     const [axiosSecure] = useAxiosSecure();
 
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         enabled: !loading,
-        // queryFn: async () => {
-        //     const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
-        //         headers: {
-        //             authorization: `bearer ${token}`
-        //         }
-        //     });
-        //     return response.json();
-        // },
         queryFn: async () => {
             const res = await axiosSecure(`/carts?email=${user?.email}`);
             console.log('res from axios', res);
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,17 +2,22 @@ import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import CheckoutForm from "./CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import useCart from "../../../hooks/useCart";
 
 // TODO: provide publishable key
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const price = parseFloat(total.toFixed(2));
+
     return (
         <div className="w-full ml-20">
             <SectionTitle subHeading={"Please process"} heading={"Payment"} />
             <h2 className="text-3xl mb-10">Pay with Stripe</h2>
             <Elements stripe={stripePromise}>
-                <CheckoutForm />
+                <CheckoutForm price={price}/>
             </Elements>
         </div>
     );
